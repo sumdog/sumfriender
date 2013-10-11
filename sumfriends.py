a2c03ad2daa28d1614436f2d2ecf7759a07e10b7
Sumit Khanna <sumit@catalyst.net.nz>
2013-10-11 16:33:20 +1300

    It (mostly) works again in pure Python

--- sumfriends.py
+++ sumfriends.py
@@ -1,7 +1,73 @@
 #!/usr/bin/env python3
 
 import time
-from subprocess import Popen,PIPE
+
+import configparser
+import urllib.request
+import urllib.parse
+import webbrowser
+import json
+
+class Facebook(object):
+
+  def __init__(self,config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    self.fb_app = config.get('FB_API','APP_ID')
+    self.fb_key = config.get('FB_API','APP_KEY')
+    self.redirect_uri = config.get('FB_API','REDIRECT_URI')
+    self.oauth_token = config.get('FB_API','OAUTH_TOKEN')
+
+  def requires_auth(self):
+    return self.oauth_token.strip() == '' 
+
+  
+  def __fb_url(self,path,vars):
+    return 'https://graph.facebook.com/{0}?{1}'.format(
+      path,
+      urllib.parse.urlencode(vars))
+
+  def __make_request(self,url):
+    #print(url)
+    with urllib.request.urlopen(url) as html:
+      return str( html.read() , 'ascii' , 'ignore' )
+
+  def login(self):
+    webbrowser.open(self.__fb_url('oauth/authorize',
+      { 'type' : 'user_agent' , 
+        'client_id' : self.fb_app , 
+        'redirect_uri' : self.redirect_uri,
+        'response_type' : 'token' ,
+        'scope' : 'user_friends'
+      }
+    ))
+
+  def __friends_as_dict(self,obj):
+    ret = {}
+    for f in obj:
+      ret[f['id']] = f['name']
+    return ret
+
+  def get_friends(self):
+    obj = json.loads((self.__make_request(self.__fb_url('me/friends',
+      { 'access_token' : self.oauth_token }
+    ))))
+    friends = self.__friends_as_dict(obj['data'])
+    while 'paging' in obj and 'next' in obj['paging']:
+      obj = json.loads(self.__make_request(obj['paging']['next']))
+      if len(obj['data']) > 0:
+        friends.update(self.__friends_as_dict(obj['data']))
+    return friends
+
+  def user_active(self,uid):
+    try:
+      obj = json.loads(self.__make_request(self.__fb_url(uid,
+        { 'access_token' : self.oauth_token }
+      )))
+      return 'id' in obj
+    except urllib.error.HTTPError:
+      return False
+
 
 def load_old_friends(data):
   oldfriend = open(data,'r')
@@ -11,6 +77,7 @@ def load_old_friends(data):
     data[parts[0]] = " ".join(parts[1:]).strip()
   return data
 
+"""
 def load_current_friends():
   fbcmd = Popen(['fbcmd','friends'],shell=False,bufsize=1,stdin=PIPE,stdout=PIPE)
   data = {}
@@ -19,6 +86,7 @@ def load_current_friends():
     if parts[0].strip() != 'ID':
       data[parts[0]] = ' '.join(parts[1:]).strip()
   return data
+  """
     
   
 
@@ -30,10 +98,16 @@ def save_new_friends():
   pass
 
 if __name__ == '__main__':
-  
-  old_friends = load_old_friends('friends.txt')
-  cur_friends = load_current_friends()
-  #print(old_friends)
-  for uid in old_friends:
-    if uid not in cur_friends:
-      print("Friend {0} ({1}) is no longer in your friends list".format(old_friends[uid],uid))
+
+  fb = Facebook("sumfriender.config")
+  if fb.requires_auth():
+    print("You need a login key. Copy your access token to the OAUTH_TOKEN field in the configuration file.")
+    fb.login()
+  else:
+    cur_friends = fb.get_friends()  
+    old_friends = load_old_friends('friends.txt')
+
+    for uid in old_friends:
+      if uid not in cur_friends:
+        status = 'is no longer in your friends list' if fb.user_active(uid) else 'has been deactivated'
+        print("Friend {0} ({1}) {2}".format(old_friends[uid],uid,status))
